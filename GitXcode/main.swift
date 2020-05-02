e75bf0d49744dba4afeebf6c46ffb14394e9d7dd
Hangil Lee <hangil0216@gmail.com>
2020-05-02 15:40:35 +0900

    Initial Commit

---
+++ GitXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode
+//
+//  Created by MAC09 on 2020/05/02.
+//  Copyright © 2020 Hangil Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
